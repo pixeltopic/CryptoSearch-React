cba5bf66c185c822d058108f5fb2a3c0ac304359
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-02-04 02:27:24 -0800

    Converted tab indent to spaces

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ import InputForm from "./input_components/input_form";
 // cd C:\Users\xmobl\Documents\GitRepos\cryptosearch-react
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {
             inputFormData: {searchInput: "", exchangeInput: "", curInput: ""}
         };
-	}
-	// add fetch class below inputform, which will pass in inputFormData as a prop.
-	render() {
-		// console.log(this.state.inputFormData);
-		return (
-			<div className="App">
-				<header className="App-header">
+    }
+    // add fetch class below inputform, which will pass in inputFormData as a prop.
+    render() {
+        // console.log(this.state.inputFormData);
+        return (
+            <div className="App">
+                <header className="App-header">
                     {/*<img src={logo} className="App-logo" alt="logo" />*/}
-					<h1 className="App-title">CryptoSearch-React</h1>
-					<h2 className="App-subtitle">React.js rewrite of CryptoSearch</h2>
-				</header>
-				
-				<InputForm onInputChange={ 
-					allInputs => this.setState({inputFormData : allInputs}, 
-					() => console.log(this.state.inputFormData))}/>
-					
-			</div>
-		);
-	}
+                    <h1 className="App-title">CryptoSearch-React</h1>
+                    <h2 className="App-subtitle">React.js rewrite of CryptoSearch</h2>
+                </header>
+                
+                <InputForm onInputChange={ 
+                    allInputs => this.setState({inputFormData : allInputs}, 
+                    () => console.log(this.state.inputFormData))}/>
+                    
+            </div>
+        );
+    }
 }
 
 export default App;
